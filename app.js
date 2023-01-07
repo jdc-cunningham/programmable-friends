db8e41f4684bbb5fc455e28e68eabb53dc95647a
Jacob Cunningham <jcun4128@gmail.com>
2023-01-07 14:47:16 -0600

    start working on twilio server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
+
+
+
+// CORs
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(
+  bodyParser.json(),
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get('/', (req, res) => {
+  res.status(200).send('online');
+});
+
+app.get('/programmable-friends', (req, res) => {
+  console.log(req);
+  res.status(200);
+});
+
+app.listen(port, () => {
+  console.log(`App running... on port ${port}`);
+});
