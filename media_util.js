90e6c59caddf19021b53bc788fd1268a4bcc8d1f
Jacob Cunningham <jcun4128@gmail.com>
2023-01-07 16:38:08 -0600

    test this

---
+++ media_util.js
@@ -0,0 +1,71 @@
+const exec = require('child_process').exec;
+const https = require('https'); // or 'https' for https:// URLs
+const fs = require('fs');
+const { twilioSend } = require('./twilio');
+
+const appBasePath = process.cwd();
+
+// https://superuser.com/questions/1009969/how-to-extract-a-frame-out-of-a-video-using-ffmpeg
+const getImageFromVideo = (videoPath, frameCount = 1) => { // frameCount means multiple, future nice to have
+  const cmd = `ffmpeg -i ${videoPath} -vf "select=1" -vframes 1 ${appBasePath}/images/out.png`;
+
+  exec(cmd, function (error, stdout, stderr) {
+    if (error) {
+      console.log(error);
+      return false;
+    }
+    if (stderr) {
+      console.log(stderr);
+      return false;
+    }
+    return true;
+  });
+}
+
+// https://www.twilio.com/docs/sms/accepted-mime-types
+// assumes no audio type is used, rare for me
+const getFilePath = (mediaType) => {
+  // this assumes last part of mimetype = file extension
+  // I was initially iterating over a list of mimetypes but seems redundant
+
+  if (mediaType.indexOf('video') !== -1) {
+    return `${appBasePath}/videos/file.${mediaType.split('/')[1]}`;
+  } else {
+    return `${appBasePath}/images/file.${mediaType.split('/')[1]}`;
+  }
+}
+
+// https://stackoverflow.com/a/11944984/2710227
+const remoteFileToDisk = async (mediaUrl, type) => {
+  const filepath = getFilePath(type);
+  const file = fs.createWriteStream(filepath);
+
+  return new Promise(resolve => {
+    https.get(mediaUrl, function(response) {
+      response.pipe(file);
+  
+      // after download completed close filestream
+      file.on('finish', () => {
+          file.close();
+          resolve(fileName);
+      });
+
+      file.on('error', (err) => {
+        console.log(err);
+        resolve(false);
+      })
+    });
+  });
+}
+
+const deleteFile = filePath => {
+  fs.unlink(filePath, (err => {
+    if (err) twilioSend('failed to delete file');
+  }));
+}
+
+module.exports = {
+  getImageFromVideo,
+  remoteFileToDisk,
+  deleteFile,
+};
--- methods.js
+++ methods.js
@@ -4,7 +4,7 @@ dotenv.config({ path: __dirname + '/.env' });
 const { getImageLabels } = require('./google_cloud_vision');
 const { msgFriend } = require('./openai_friend_chat');
 const { twilioSend } = require('./twilio');
-const { getImageFromVideo } = require('./video_util');
+const { getImageFromVideo, remoteFileToDisk, deleteFile } = require('./media_util');
 
 const verifySender = body => {
   if (body.From === process.env.MY_NUMBER) {
@@ -34,16 +34,20 @@ const processMsg = async (contentType, body) => {
   if (contentType === 'text') {
     response = await msgFriend(Body);
   } else if (contentType === 'image') {
-    const imgContext = getImageLabels(body.mediaUrl0);
+    const localImg = await remoteFileToDisk(body.mediaUrl0, MediaContentType0);
+    const imgContext = getImageLabels(localImg);
 
     if (imgContext) {
       response = await msgFriend(Body + '\n\n' + imgContext);
+      deleteFile(localImg); // not a big deal but avoid buildup/security
     }
   } else {
+    const localVid = await remoteFileToDisk(body.mediaUrl0, MediaContentType0);
     const vidContext = getImageFromVideo(body.mediaUrl0);
 
     if (vidContext) {
       response = await msgFriend(Body + '\n\n' + vidContext);
+      deleteFile(localVid);
     }
   }
   
