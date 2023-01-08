3ba756bef9f7e39b2669712067efe5406362f58c
Jacob Cunningham <jcun4128@gmail.com>
2023-01-07 18:57:58 -0600

    fix this

--- media_util.js
+++ media_util.js
@@ -37,9 +37,10 @@ const getFilePath = (mediaType) => {
 }
 
 // https://stackoverflow.com/a/40554947/2710227
-const download = async (uri, filename) => {
+const download = (uri, filename) => {
   var protocol = url.parse(uri).protocol.slice(0, -1);
   var deferred = q.defer();
+
   var onError = function (e) {
       fs.unlink(filename);
       deferred.reject(e);
